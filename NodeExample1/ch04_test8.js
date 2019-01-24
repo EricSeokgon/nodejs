a3ae475fb0ad683022adf7bf8f402f408363296b
hadesyi <leesk55@gmail.com>
2019-01-24 13:33:09 +0900

    파일을 직접 열고 닫으면서 읽거나 쓰기

---
+++ NodeExample1/ch04_test8.js
@@ -0,0 +1,16 @@
+var fs = require('fs');
+
+fs.open('./output.txt','w', function (err, fs) {
+    if (err) throw err;
+
+    var buf = new Buffer('안녕!\n');
+    fs.write(fd,buf,0,buf.length,null,function (err,written,vuffer) {
+        if(err) throw err;
+
+        console.log(err, written, buffer);
+
+        fs.close(fd,function () {
+            console.log('파일 열고 데이터 쓰고 파일 닫기 완료.');
+        });
+    });
+});
