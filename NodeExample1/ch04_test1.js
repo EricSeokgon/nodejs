eb3309ffe2cd9887c5820d94ad0026a80fbc33bb
hadesyi <leesk55@gmail.com>
2019-01-24 10:37:13 +0900

    요청 파라미터 확인

--- NodeExample1/ch04_test1.js
+++ NodeExample1/ch04_test1.js
@@ -4,3 +4,8 @@ var curURL = url.parse('https://m.search.naver.com/search.naver?query=steve+jobs
 var curStr = url.format(curURL);
 console.log('주소 문자열 : %s', curStr);
 console.dir(curURL);
+
+var querystring = require('querystring');
+var param = querystring.parse(curURL.query);
+console.log('요청 파라미터 중 query의 값 : %s', param.query);
+console.log('원본 요청 파라미터 : %s', querystring.stringify(param));
