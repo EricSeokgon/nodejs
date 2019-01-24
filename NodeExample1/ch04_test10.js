ffb9e7b8c58ae57e191632c7b29ac5147ac287c6
hadesyi <leesk55@gmail.com>
2019-01-24 14:52:55 +0900

    버퍼 객체 사용하는 방법

--- NodeExample1/ch04_test10.js
+++ NodeExample1/ch04_test10.js
@@ -6,7 +6,7 @@ console.log('첫 번째 버퍼의 문자열 : %s', buffer1.toString());
 var buffer2 = new Buffer('안녕 2!', 'utf8');
 console.log('두 번째 버퍼의 문자열 : %s', 'utf8');
 
-console.log('버퍼 객체의 타입 : %s', Buffer.isButter(buffer1));
+console.log('버퍼 객체의 타입 : %s', Buffer.isBuffer(buffer1));
 
 var byteLen = Buffer.byteLength(output);
 var str1 = buffer1.toString('utf8', 0, byteLen);
@@ -16,4 +16,4 @@ buffer1.copy(buffer2, 0, 0, len);
 console.log('두 번째 버퍼에 복사한 후의 문자열 : %s', buffer2.toString('utf8'));
 
 var buffer3 = Buffer.concat([buffer1, buffer2]);
-console.log('두 개의 버퍼를 붙인 후의 문자열 : %s', buffer3.toLocaleLowerCase('utf8'));
+console.log('두 개의 버퍼를 붙인 후의 문자열 : %s', buffer3.toString('utf8'));
