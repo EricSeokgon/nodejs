60a3ca7c3a6a24570ce87677faae106dab7b0bc0
hadesyi <leesk55@gmail.com>
2019-01-25 15:34:31 +0900

    오류 페이지 보여 주기

--- ExpressExample/app8_02.js
+++ ExpressExample/app8_02.js
@@ -24,17 +24,19 @@ app.use('/public', static(path.join(__dirname, 'public')));
 var router = express.Router();
 
 // 라우팅 함수 등록
-router.route('/process/login').post(function (req, res) {
-    console.log('/process/login 처리함.');
+router.route('/process/login/:name').post(function (req, res) {
+    console.log('/process/login/:name 처리함.');
+    var paramName = req.params.name;
 
     var paramId = req.body.id || req.query.id;
     var paramPassword = req.body.password || req.query.password;
 
     res.writeHead('200', {'Content-Type': 'text/html;charset=utf8'});
     res.write('<h1>Express 서버에서 응답한 결과입니다.</h1>');
+    res.write('<div><p>Param name :' + paramName + '</p></div>');
     res.write('<div><p>Param id :' + paramId + '</p></div>');
     res.write('<div><p>Param password :' + paramPassword + '</p></div>');
-    res.write("<br><br><a href='/public/login2.html'>로그인 페이지로 돌아가기</a>");
+    res.write("<br><br><a href='/public/login3.html'>로그인 페이지로 돌아가기</a>");
     res.end();
 });
 
@@ -60,11 +62,6 @@ app.use('/', function (req, res, next) {
 
     res.writeHead('200', {'Content-Type': 'text/html;charset=utf8'});
     res.end('<h1>Express 서버에서 ' + req.user + ' 가 응답한 결과입니다.</h1>');
-});
-
-//등록되지 않은 패스에 대해 페이지 오류 응답
-app.all('*', function (req, res) {
-    res.status(404).send('<h1>ERROR = 페이지를 찾을 수 없습니다.</h1>');
 });
 //Express 서버 시작
 http.createServer(app).listen(3000, function () {
