a5131c7b446d1d62ff0ff3e02510c85401689c2b
hadesyi <leesk55@gmail.com>
2019-01-25 10:09:56 +0900

    로그파일 기록 수정

--- NodeExample1/ch04_test15.js
+++ NodeExample1/ch04_test15.js
@@ -1,31 +1,38 @@
 var winston = require('winston');
 var winstonDaily = require('winston-daily-rotate-file');
-var moment = require('moment');
 
-function timeStampFormat() {
-    return moment().format('YYYY-MM-DD HH:mm:ss.SSS ZZ');
-};
-var logger = new (winston.Logger)({
+var logger = winston.createLogger({
+    level: 'debug',
+    format: winston.format.simple(),
     transports: [
-        new (winstonDaily)({
-            name: 'info-file',
-            filename: './log/server',
-            datePattern: '_yyyy-MM-dd.log',
-            colorize: false,
-            maxsize: 50000000,
-            maxFiles: 1000,
-            level: 'info',
-            showLevel: true,
-            json: false,
-            timestamp: timeStampFormat
-        }),
         new (winston.transports.Console)({
-            name: 'exception-console',
-            colorizr: true,
-            level: 'debug',
-            showLevel: true,
-            json: false,
-            timeStamp: timeStampFormat
+            colorize: true
+        }),
+        new (winstonDaily)({
+            filename: './log/server_%DATE%.log',
+            maxSize: '10m',
+            datePattern: 'YYYY-MM-DD'
         })
     ]
 });
+
+var fs = require('fs');
+
+var inname = './output.txt';
+var outname = './output2.txt';
+
+fs.exists(outname, function (exists) {
+    if (exists) {
+        fs.unlink(outname, function (err) {
+            if (err) throw err;
+            logger.info('기존 파일 [' + outname + '] 삭제함.');
+
+        });
+    }
+    var infile = fs.createReadStream(inname, {flags: 'r'});
+    var outfile = fs.createWriteStream(outname, {flags: 'w'});
+
+    infile.pipe(outfile);
+    logger.info('파일 복사 [' + inname + '] -> [' + outname + ']');
+});
+
