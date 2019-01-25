d8bc04dc79c423884639b9057841644971a4e89e
hadesyi <leesk55@gmail.com>
2019-01-25 14:30:10 +0900

    익스프레스에서 요청 객체에 추가한 헤더와 파라미터 알아보기

---
+++ ExpressExample/app5.js
@@ -0,0 +1,23 @@
+//Express 기본 모듈 불러오기
+var express = require('express')
+    , http = require('http');
+
+// 익스프레스 객체 생성
+var app = express();
+
+app.use(function (req, res, next) {
+    console.log('첫 번째 미들웨어에서 요청을 처리함.');
+
+    res.redirect('http://googole.co.kr');
+});
+
+app.use('/', function (req, res, next) {
+    console.log('두 번째 미들웨어에서 요청을 처리함.');
+
+    res.writeHead('200', {'Content-Type': 'text/html;charset=utf8'});
+    res.end('<h1>Express 서버에서 ' + req.user + ' 가 응답한 결과입니다.</h1>');
+});
+//Express 서버 시작
+http.createServer(app).listen(3000, function () {
+    console.log('Express 서버가 3000번 포트에서 시작됨.');
+});
