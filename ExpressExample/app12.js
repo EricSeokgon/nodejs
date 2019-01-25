06be4f62239e0864db649ad0b2f82de05f664473
hadesyi <leesk55@gmail.com>
2019-01-26 01:09:44 +0900

    상품페이지

--- ExpressExample/app12.js
+++ ExpressExample/app12.js
@@ -42,7 +42,56 @@ router.route('/process/product').get(function (req, res) {
         res.redirect('/public/login2.html');
     }
 });
+// 로그인 라우팅 함수 - 로그인 후 세션 저장함
+router.route('/process/login').post(function (req, res) {
+    console.log('/process/login 호출됨.');
 
+    var paramId = req.body.id || req.query.id;
+    var paramPassword = req.body.password || req.query.password;
+
+    if (req.session.user) {
+        //이미 로그인된 상태
+        console.log('이미 로그인되어 상품 페이지로 이동합니다.');
+        res.redirect('/public/product.html');
+    } else {
+        //세션 저장
+        req.session.user = {
+            id: paramId,
+            name: '소녀시대',
+            authorized: true
+        };
+
+        res.writeHead('200', {'Content-Type': 'text/html;charset=utf8'});
+        res.write('<h1>로그인 성공</h1>');
+        res.write('<div><p>Param id :' + paramId + '</p></div>');
+        res.write('<div><p>Param password :' + paramPassword + '</p></div>');
+        res.write("<br><br><a href='/process/product'>상품 페이지로 이동하기</a>");
+        res.end();
+    }
+});
+
+//로그아웃 라우팅 함수 - 로그아웃 후 세션 삭제함
+router.route('/process/logout').get(function (req, res) {
+    console.log('/process/logout 호출됨.');
+
+    var paramId = req.body.id || req.query.id;
+    var paramPassword = req.body.password || req.query.password;
+
+    if (req.session.user) {
+        //이미 로그인된 상태
+        console.log('로그아웃합니다.');
+
+        req.session.destroy(function (err) {
+            if (err) {
+                throw err;
+            }
+        });
+    } else {
+        //로그인 안된 상태
+        console.log('아직 로그인되어 있지 않습니다.')
+        res.redirect('/public/login2.html');
+    }
+});
 // 라우터 객체를 app 객체에 등록
 app.use('/', router);
 
